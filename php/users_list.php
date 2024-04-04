62f6a522c99ce4b1c5df447832f4f192c0944549
kael <kaelscott8@gmail.com>
2024-04-04 11:43:35 -0300

    add users list

---
+++ php/users_list.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<title>W3.CSS</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+
+<div class="w3-container w3-responsive">
+  <h2>Hoverable Table</h2>
+
+  <table  class="w3-table-all w3-hoverable ">
+    <thead>
+      <tr class="w3-light-grey">
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Email</th>
+        <th colspan=2>Ações</th>
+      </tr>
+    </thead>
+    <tr>
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>94</td>
+      <td>50</td>
+      <td>50</td>
+    </tr>
+    <tr>
+      <td>Eve</td>
+      <td>Jackson</td>
+      <td>94</td>
+      <td>50</td>
+      <td>94</td>
+    </tr>
+    <tr>
+      <td>Adam</td>
+      <td>Johnson</td>
+      <td>67</td>
+      <td>50</td>
+      <td>67</td>
+    </tr>
+
+  </table>
+</div>
+
+</body>
+</html>
