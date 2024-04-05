6447ccb79d3d9b4ab1a0becee3946eb74f8e0281
kael <kaelscott8@gmail.com>
2024-04-04 21:13:34 -0300

    add delete button

--- php/users_list.php
+++ php/users_list.php
@@ -1,24 +1,52 @@
 <!DOCTYPE html>
 <html>
-<title></title>
+<title>Lista de usuários</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-ios.css">
+
+<style>
+    a {
+        text-decoration: none;
+    }
+</style>
+
 <body>
 
-<div class="w3-container">
-  <h2>Avatar List</h2>
-  <p>You can combine w3-ul and the w3-bar classes to create an avatar list:</p>
-  <ul class="w3-ul w3-card-4">
-    <li class="w3-bar">
-      <span class="w3-bar-item w3-button w3-white w3-xlarge w3-right">×</span>
-      <img src="../images/default_icon.png" class="w3-bar-item w3-circle w3-hide-small" style="width:85px">
-      <div class="w3-bar-item">
-        <span class="w3-large">123</span><br>
-        <span>123@123</span>
-      </div>
-    </li>
-  </ul>
-</div>
+    <?php include("../Conexão.php");
+    $sql = "SELECT id_user, nome_user, email FROM usuarios";
+    $result = $conn->query($sql);
+    ?>
 
+    <div class="w3-container">
+        <h2>Usuários</h2>
+
+        <ul class="w3-ul w3-card-4">
+            <?php
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $id_user = $row['id_user'];
+            ?>
+                    <li class="w3-bar">
+                        <img src="../images/default_icon.png" class="w3-bar-item w3-circle w3-hide-small w3-margin-top w3-margin-bottom" style="width:85px">
+                        <div class="w3-bar-item w3-margin-top w3-margin-bottom">
+                            <span class="w3-large"><?php echo $row['nome_user'] ?></span> <br>
+                            <span><?php echo $row['email'] ?></span>
+                        </div>
+
+                        <form action="users_del_php.php" method="post">
+                            <input type="hidden" name="id_user" value="<?php echo $id_user; ?>">
+                            <button class="w3-xlarge w3-right w3-button w3-circle w3-ios-red w3-hover-red w3-margin-left w3-margin">
+                                ×
+                            </button>
+                        </form>
+
+                    </li>
+            <?php
+                }
+            }
+            ?>
+        </ul>
+    </div>
 </body>
 </html>
