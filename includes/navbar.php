70f7ff8a1afb1565e03ccbe2f9e8a364d5fcf9e4
kael <kaelscott8@gmail.com>
2024-04-09 18:13:27 -0300

    add acivate class on navbar links

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,19 +1,22 @@
+<?php $currentPage = basename($_SERVER['SCRIPT_NAME']); ?> <!-- pega o nome do arquivo atual -->
+
 <!DOCTYPE HTML>
 <html>
-	<head>
+
+<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>flavour Hunter</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" />
-	<meta name="keywords"/>
+	<meta name="keywords" />
 	<meta name="author" />
 
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
+	<meta property="og:title" content="" />
+	<meta property="og:image" content="" />
+	<meta property="og:url" content="" />
+	<meta property="og:site_name" content="" />
+	<meta property="og:description" content="" />
 	<meta name="twitter:title" content="" />
 	<meta name="twitter:image" content="" />
 	<meta name="twitter:url" content="" />
@@ -31,13 +34,14 @@
 
 	<script src="js/modernizr-2.6.2.min.js"></script>
 
-	</head>
-	<body>
+</head>
+
+<body>
 
 	<div class="fh5co-loader"></div>
 
 	<div id="page">
-	<nav class="fh5co-nav" role="navigation">
+		<nav class="fh5co-nav" role="navigation">
 			<div class="container">
 				<div class="row">
 					<div class="col-xs-12 text-center logo-wrap">
@@ -45,10 +49,19 @@
 					</div>
 					<div class="col-xs-12 text-center menu-1 menu-wrap">
 						<ul>
-							<li class="active"><a href="index.php">Home</a></li>
-							<li><a href="restaurantes.php">Restaurantes</a></li>
-							<li><a href="about.php">About</a></li>
-							<li><a href="contato.php">Contato</a></li>
+							<!-- analisa qual é a página atual e adiciona a classe active -->
+							<li <?php if ($currentPage === 'index.php') : ?>class="active" <?php endif; ?>>
+								<a href="index.php">Home</a>
+							</li>
+							<li <?php if ($currentPage === 'restaurantes.php') : ?>class="active" <?php endif; ?>>
+								<a href="restaurantes.php">Restaurantes</a>
+							</li>
+							<li <?php if ($currentPage === 'about.php') : ?>class="active" <?php endif; ?>>
+								<a href="about.php">About</a>
+							</li>
+							<li <?php if ($currentPage === 'contato.php') : ?>class="active" <?php endif; ?>>
+								<a href="contato.php">Contato</a>
+							</li>
 							<li><a href="cadastrar.php">Cadastrar-se</a></li>
 							<li><a href="php/users_list.php">Lista de Usuários</a></li>
 						</ul>
@@ -56,4 +69,4 @@
 				</div>
 
 			</div>
-	</nav>
+		</nav>
