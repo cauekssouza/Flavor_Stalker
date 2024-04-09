efd000cd139c2736a672f4da68acb1ec4f8b9128
kael <kaelscott8@gmail.com>
2024-04-09 16:33:07 -0300

    Convert index.html to PHP for better dynamic content

---
+++ includes/footer.php
@@ -0,0 +1,61 @@
+<footer id="fh5co-footer" role="contentinfo" class="fh5co-section">
+		<div class="container">
+			<div class="row row-pb-md">
+				<div class="col-md-4 fh5co-widget">
+					<h4>Flavour hunter</h4>
+					<p>É uma plataforma de promoção gastronômica, destacando suas especialidades culinárias e únicas, é voltada para explorar a gastronomia local, fornecer informações detalhadas sobre o restaurante e é uma ferramenta de feedback e interação.</p>
+				</div>
+				<div class="col-md-2 col-md-push-1 fh5co-widget">
+					<h4>Links</h4>
+					<ul class="fh5co-footer-links">
+						<li><a href="#">Home</a></li>
+
+						<li><a href="#">Restaurantes</a></li>
+
+					</ul>
+				</div>
+
+
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; 2024 Flavour Hunter All Rights Reserved.</small>
+						<small class="block">Designed by Lucas Zacarias</small>
+					</p>
+					<p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter2"></i></a></li>
+							<li><a href="#"><i class="icon-facebook2"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin2"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+	</div>
+
+
+	<script src="js/jquery.min.js"></script>
+
+	<script src="js/jquery.easing.1.3.js"></script>
+
+	<script src="js/bootstrap.min.js"></script>
+
+	<script src="js/jquery.waypoints.min.js"></script>
+
+	<script src="js/jquery.stellar.min.js"></script>
+
+	<script src="js/jquery.flexslider-min.js"></script>
+
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
