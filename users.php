d739ef58fe034ab88d969f980195a5cc60ecb9a0
kael <kaelscott8@gmail.com>
2024-04-10 22:27:32 -0300

    add users page

---
+++ users.php
@@ -0,0 +1,109 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>flavour Hunter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" />
+    <meta name="keywords" />
+    <meta name="author" />
+
+    <meta property="og:title" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:description" content="" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,300i,400,400i,500,600i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/flexslider.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/modernizr-2.6.2.min.js"></script>
+
+</head>
+
+<body>
+
+    <div class="fh5co-loader"></div>
+
+    <div id="page">
+        <div class="container">
+
+            <div class="row ">
+                <div class="col col-md-4 ">
+
+                    <div class="fh5co-blog animate-box">
+                        <a href="#" class="blog-bg" style="background-image: url(images/default_icon.png);"></a>
+                        <div class="blog-text">
+                            <span class="posted_on">Feb. 15th 2016</span>
+                            <h3><a href="#">Barolo Trattoria</a></h3>
+                            <p>Otimo restaurante!</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <p><strong>Column 2</strong></p>
+                    <p>Duis placerat sed libero ut viverra. Sed gravida aliquet nisi vitae porta.</p>
+                </div>
+            </div>
+
+            <div class="row ">
+                <div class="col col-md-4">
+                    <button class="btn btn-primary"><strong>Editar Perfil</strong></button>
+                </div>
+            </div>
+
+            <div class="row ">
+                <div class="col col-md-4">
+                    <button class="btn btn-primary"><strong>Favoritos</strong></button>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+        </div>
+    </div>
+
+
+
+
+
+    <div class="gototop js-top">
+        <a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+    </div>
+
+
+    <script src="js/jquery.min.js"></script>
+
+    <script src="js/jquery.easing.1.3.js"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/jquery.waypoints.min.js"></script>
+
+    <script src="js/jquery.stellar.min.js"></script>
+
+    <script src="js/jquery.flexslider-min.js"></script>
+
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
