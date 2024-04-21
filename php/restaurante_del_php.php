9cf5d66e9f2508635be25c9ccc4d19d36aea7e75
kael <kaelscott8@gmail.com>
2024-04-21 17:00:35 -0300

    add restaurantes list

---
+++ php/restaurante_del_php.php
@@ -0,0 +1,19 @@
+<?php include("../Conexão.php");
+
+$id_restaurante = $_POST["id_restaurante"];
+
+$sql = "DELETE FROM restaurantes WHERE id_restaurante = '$id_restaurante'";
+$result = $conn->query($sql);
+
+if ($result === TRUE) {
+    header("Location: ../restaurantes_list.php");
+    exit();
+} else {
+?>
+
+    <script>
+        alert("falha");
+    </script>
+
+<?php
+}
---
+++ restaurantes_list.php
@@ -0,0 +1,129 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>flavour Hunter</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" />
+    <meta name="keywords" />
+    <meta name="author" />
+
+    <meta property="og:title" content="" />
+    <meta property="og:image" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="" />
+    <meta property="og:description" content="" />
+    <meta name="twitter:title" content="" />
+    <meta name="twitter:image" content="" />
+    <meta name="twitter:url" content="" />
+    <meta name="twitter:card" content="" />
+
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:300,300i,400,400i,500,600i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Satisfy" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/flexslider.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/modernizr-2.6.2.min.js"></script>
+
+</head>
+
+<body>
+
+    <div class="fh5co-loader"></div>
+
+    <?php
+    include("Conexão.php");
+
+
+    $sql = "SELECT id_restaurante, nome, dono FROM restaurantes";
+    $result = $conn->query($sql);
+    ?>
+
+    <div class="container text-center mt-5 ">
+        <h1 class="text-white">Restaurantes</h1>
+        <table class="table table-hover border-dark table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Dono</th>
+                    <th scope="col">Deletar</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider ">
+                <?php
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $id_restaurante = $row['id_restaurante'];
+                ?>
+                        <tr>
+                            <th scope="row"><?php echo $id_restaurante ?></th>
+                            <td><?php echo $row['nome'] ?></td>
+                            <td><?php echo $row['dono'] ?></td>
+                            <td>
+                                <form action="php/restaurante_del_php.php" method="post">
+                                    <input type="hidden" name="id_restaurante" value="<?php echo $id_restaurante; ?>">
+                                    <button type="submit" class="btn-close" aria-label="Close"></button>
+                                </form>
+
+                            </td>
+                        </tr>
+                <?php
+                    }
+                } else {
+                    // Se não houver usuários no banco de dados
+                    echo "<tr><td colspan='5'>Nenhum usuário encontrado</td></tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="gototop js-top">
+        <a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
+    </div>
+
+
+    <script src="js/jquery.min.js"></script>
+
+    <script src="js/jquery.easing.1.3.js"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+
+    <script src="js/jquery.waypoints.min.js"></script>
+
+    <script src="js/jquery.stellar.min.js"></script>
+
+    <script src="js/jquery.flexslider-min.js"></script>
+
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
