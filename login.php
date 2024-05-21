4288409dd273201e4cf21a2ad10b37b99b46e4f6
kael <kaelscott8@gmail.com>
2024-05-20 22:57:12 -0300

    change login appereance

--- login.php
+++ login.php
@@ -1,63 +1,49 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
 
+<!DOCTYPE html>
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flavour_Stalker</title>
-    <link rel="stylesheet" href="login.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <title>Login</title>
+    <!-- Incluindo estilos da navbar -->
+    <?php include("includes/navbar.php"); ?>
 </head>
-
 <body>
-    <div class="container">
-        <div class="content">
-            <div class="first-content">
-                <div class="first-colunn">
-                    <h2 class="tittle"><i class="fas fa-right-to-bracket icon-modify"></i>Login</h2>
-                    <?php session_start();
-                    if (isset($_SESSION["error"])) {
-                        echo '
-                            <div class="w3-panel w3-pale-red w3-border">
-                                <p>'  . $_SESSION["error"] . '</p>
-                            </div>
-                        ';
-                        unset($_SESSION["error"]);
-                    }
-                    ?>
-
-                    <form method="POST" name="formLogin" action="php/login_php.php" class="form">
-
-                        <div class="label-input">
-                            <i class="fas fa-user icon-modify"></i>
-                            <input name="txtEmail" type="text" placeholder="Email">
-                        </div>
-
-                        <div class="label-input">
-                            <i class="fas fa-lock icon-modify"></i>
-                            <input name="txtSenha" type="password" id="senha" placeholder="Senha">
-                        </div>
-
-                        <input type="submit" value="Entrar" class="btn">
-                    </form>
-                    
-
-                    <div class="social-media">
-                        <div class="btn">
-                            <a href="senha.php" class="btn-primary">Esqueci Minha Senha</a>
-                        </div>
-                        <div class="btn">
-                            <a href="cadastrar.php" class="btn-primary">Cadastre-se</a>
-                        </div>
-                        <div class="btn">
-                        <button class="btn-primary" onclick="window.location.href='index.php'">Voltar</button>
-                        </div>
+    <!-- Conteúdo da página -->
+    <br><br><br><br><br>
+    <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+        <div class="row align-items-center g-lg-5 py-5">
+            <div class="col-lg-7 text-center text-lg-start">
+                <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">Entre como cliente já cadastrado</h1>
+                <p class="col-lg-10 fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam deserunt quas optio voluptatum nobis pariatur, est amet, adipisci perspiciatis sapiente nihil excepturi numquam laborum repellat alias corporis ea veritatis quisquam?</p>
+            </div>
+            <div class="col-md-10 mx-auto col-lg-5">
+            <?php
+                if(isset($_SESSION["error"])) {
+                    echo '<div class="alert alert-danger" role="alert">' . $_SESSION["error"] . '</div>';
+                    unset($_SESSION["error"]);
+                }
+            ?>
+                <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary text-dark" name="formLogin" method="POST" action="php/login_php.php">
+                    <div class="mb-3">
+                        <label for="floatingInput" class="form-label">Email</label>
+                        <input type="email" class="form-control form-control-lg" id="floatingInput" placeholder="name@example.com" name="txtEmail"  autocomplete="off">
                     </div>
-                </div>
+                    <div class="mb-3">
+                        <label for="floatingPassword" class="form-label">Senha</label>
+                        <input type="password" class="form-control form-control-lg" id="floatingPassword" placeholder="Password" name="txtSenha">
+                    </div>
+                    <div class="checkbox mb-3">
+                        <label>
+                            <input type="checkbox" value="remember-me" name="chkRemember"> Remember me
+                        </label>
+                    </div>
+                    <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
+                    <hr class="my-4">
+                    <a href="cadastrar.php" class="text-body-secondary">Crie uma conta nova</a>
+                </form>
             </div>
         </div>
-
+    </div>
 </body>
-
 </html>
