e1ea8472204b18de51f377371376aadb8f2a237c
kael <kaelscott8@gmail.com>
2024-06-03 17:59:57 -0300

    Remove unnecessary files and code related to restaurant registration and management

--- Cad_restauranteDb/footer.php
+++
@@ -1,4 +0,0 @@
-<!-- footer.php -->
-<footer>
-    <p>&copy; 2024 Lista de Restaurantes. Todos os direitos reservados.</p>
-</footer>
--- Cad_restauranteDb/navbar.php
+++
@@ -1,7 +0,0 @@
-<!-- navbar.php -->
-<nav>
-    <ul>
-        <li><a href="index.php">Página Inicial</a></li>
-        <li><a href="list.php">Adicionar Novo Restaurante</a></li>
-    </ul>
-</nav>
