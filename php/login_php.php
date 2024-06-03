23aadffc21cc0ea891f1794e18aa1949740a8c16
kael <kaelscott8@gmail.com>
2024-06-03 17:56:44 -0300

    refactor: Improve login_php.php readability and code formatting

--- php/login_php.php
+++ php/login_php.php
@@ -6,7 +6,7 @@ $email = $_POST['txtEmail'];
 $senha = $_POST['txtSenha'];
 
 // verifica se os campos estão preenchidos
-if(empty($email) || empty($senha)) {
+if (empty($email) || empty($senha)) {
     $_SESSION["error"] = "Preencha todos os campos!";
     header('Location: ../login.php');
     exit;
@@ -16,15 +16,15 @@ $sql = "SELECT id_user, id_tipo, nome_user, email, senha, data_criacao FROM usua
 $result = $conn->query($sql);
 
 // verifica se o email existe no banco
-if($result->num_rows > 0){
-    while($row = $result->fetch_assoc()){
-        if(password_verify($senha, $row['senha'])){ // verifica se a senha está correta
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        if (password_verify($senha, $row['senha'])) { // verifica se a senha está correta
             session_start();
             $_SESSION['id_user']        = $row['id_user'];
             $_SESSION['id_tipo']        = $row['id_tipo'];
             $_SESSION['email']          = $row['email'];
             $_SESSION['nome_user']      = $row['nome_user'];
-            $_SESSION['nome_user']      = $row['nome_user'];
+            $_SESSION['senha']          = $row['senha'];
             $_SESSION['data_criacao']   = $row['data_criacao'];
             $_SESSION["loggedIn"]       = true;
 
@@ -37,4 +37,3 @@ if($result->num_rows > 0){
 session_start();
 $_SESSION["error"] = "Email ou senha incorretos"; // cria uma mensagem de erro
 header('Location: ../login.php');
-?>
